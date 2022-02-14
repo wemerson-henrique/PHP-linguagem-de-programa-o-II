4b9f03a367acf57fe08b19081c5117e5935444af
Wemerson Henrique <68334448+wemerson-henrique@users.noreply.github.com>
2022-02-14 11:18:19 -0300

    apliquei estização

--- trabalho1/paginas/formulario-usuario.php
+++ trabalho1/paginas/formulario-usuario.php
@@ -1,21 +1,51 @@
 <?php 
     include('conf-bootstrap-web.php')
 ?>
+<link rel="stylesheet" href="../css/style.css">
 <body>
-    <form action="../action/incerir-usuario.php" method="post">
-        <label for="">Nome</label>
-        <input type="text" name="nome" id="nome" class=""><br>
-        <label for="">CPF</label>
-        <input type="text" name="cpf" id="cpf"><br>
-        <label for="">Telefone</label>
-        <input type="tel" name="telefone" id="telefone"><br>
-        <label for="">Nome de Usuário</label>
-        <input type="text" name="username" id="username"><br>
-        <label for="">Senha</label>
-        <input type="password" name="senha" id="senha"><br>
-        <label for="">Email</label>
-        <input type="email" name="email" id="email"><br>
-        <label for=""></label>
-        <input type="submit" value="CADASTRAR">
-    </form>
+    <div class="card">
+        <div class="card-header">
+            <h2>Cadastro de Usuário</h2>
+        </div>
+        <div class="card-body">
+            <form action="../action/incerir-usuario.php" method="post" class="p-4">
+                <div class="row mb-4">
+                    <label for="" class="col-sm-3 col-form-label">Nome Completo</label>
+                    <div class="col-sm-9">
+                        <input type="text" name="nome" id="nome" class="form-control">
+                    </div>
+                </div>
+                <div class="row g-3 mb-4">
+                    <div class="col-sm-6">
+                        <label for="">CPF</label>
+                        <input class="form-control mt-2" type="text" name="cpf" id="cpf">
+                    </div>
+                    <div class="col-sm-6">
+                        <label for="">Telefone</label>
+                        <input class="form-control mt-2" type="tel" name="telefone" id="telefone">
+                    </div>
+                </div>
+                <hr>
+                <div class="row mb-4">
+                    <label for="" class="col-sm-3 col-form-label">Nome de Usuário</label>
+                    <div class="col-sm-9">
+                        <input class="form-control" type="text" name="username" id="username">
+                    </div>
+                </div>
+                <div class="row mb-4">
+                    <label for="" class="col-sm-3 col-form-label">Senha</label>
+                    <div class="col-sm-9">
+                        <input class="form-control" type="password" name="senha" id="senha">
+                    </div>
+                </div>
+                <div class="row mb-4">
+                    <label for="" class="col-sm-3 col-form-label">Email</label>
+                    <div class="col-sm-9">
+                        <input class="form-control" type="email" name="email" id="email">
+                    </div>
+                </div>
+                <input class="btn btn-success btn-lg mx-auto" type="submit" value="CADASTRAR">
+            </form>
+        </div>
+    </div>
 </body>
